90aeef50b67e05cb0e1079e8f0f5461c11761436
Pranay Sahith Bejgum <pranaysahith7@gmail.com>
2021-03-12 01:45:16 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,64 @@
+git clone https://github.com/k8-proxy/icap-infrastructure.git && cd icap-infrastructure
+
+# Clone ICAP SOW Version 
+ICAP_SOW_BRANCH=${ICAP_SOW_BRANCH:-main}
+git clone https://github.com/filetrust/icap-infrastructure.git -b $ICAP_SOW_BRANCH /tmp/icap-infrastructure-sow
+cp  /tmp/icap-infrastructure-sow/adaptation/values.yaml adaptation/
+cp  /tmp/icap-infrastructure-sow/administration/values.yaml administration/
+cp  /tmp/icap-infrastructure-sow/ncfs/values.yaml ncfs/
+
+
+# Create namespaces
+kubectl create ns icap-adaptation
+kubectl create ns management-ui
+kubectl create ns icap-ncfs
+
+kubectl create -n icap-adaptation secret docker-registry regcred \
+	--docker-server=https://index.docker.io/v1/ \
+	--docker-username=$DOCKER_USERNAME \
+	--docker-password=$DOCKER_PASSWORD \
+	--docker-email=$DOCKER_EMAIL
+
+# Setup rabbitMQ
+pushd rabbitmq && helm upgrade rabbitmq --install . --namespace icap-adaptation && popd
+
+# Setup icap-server
+cat >> openssl.cnf <<EOF
+[ req ]
+prompt = no
+distinguished_name = req_distinguished_name
+[ req_distinguished_name ]
+C = GB
+ST = London
+L = London
+O = Glasswall
+OU = IT
+CN = icap-server
+emailAddress = admin@glasswall.com
+EOF
+
+openssl req -newkey rsa:2048 -config openssl.cnf -nodes -keyout  /tmp/tls.key -x509 -days 365 -out /tmp/certificate.crt
+kubectl create secret tls icap-service-tls-config --namespace icap-adaptation --key /tmp/tls.key --cert /tmp/certificate.crt
+
+pushd adaptation
+kubectl create -n icap-adaptation secret generic policyupdateservicesecret --from-literal=username=policy-management --from-literal=password='long-password'
+kubectl create -n icap-adaptation secret generic transactionqueryservicesecret --from-literal=username=query-service --from-literal=password='long-password'
+kubectl create -n icap-adaptation secret generic  rabbitmq-service-default-user --from-literal=username=guest --from-literal=password='guest'
+helm upgrade adaptation --values custom-values.yaml --install . --namespace icap-adaptation
+popd
+
+# Setup icap policy management
+pushd ncfs
+kubectl create -n icap-ncfs secret generic ncfspolicyupdateservicesecret --from-literal=username=policy-update --from-literal=password='long-password'
+helm upgrade ncfs --values custom-values.yaml --install . --namespace icap-ncfs
+popd
+
+# setup management ui
+kubectl create -n management-ui secret generic transactionqueryserviceref --from-literal=username=query-service --from-literal=password='long-password'
+kubectl create -n management-ui secret generic policyupdateserviceref --from-literal=username=policy-management --from-literal=password='long-password'
+kubectl create -n management-ui secret generic ncfspolicyupdateserviceref --from-literal=username=policy-update --from-literal=password='long-password'
+
+pushd administration
+helm upgrade administration --values custom-values.yaml --install . --namespace management-ui
+popd
+
